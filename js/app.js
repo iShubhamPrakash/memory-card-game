17dcc6747e79de8b561e7fe910c26db80d87ea14
Shubham Prakash <shubham.prakash2308@gmail.com>
2018-08-15 04:02:25 +0530

    add basic code

--- js/app.js
+++ js/app.js
@@ -12,7 +12,8 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
