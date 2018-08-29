2fd2799c7a36ce628077eaead277d3f646fc664b
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-08-29 16:31:02 +0530

    Add my contact info

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+/**************************************************************************************
+ * Shubham Prakash
+ * Email: shubham.prakash2308@gmail.com
+ * LinkedIn: https: //www.linkedin.com/in/ishubhamprakash/
+ **************************************************************************************/
 
 /* A list that holds all of the cards */
 let cardList = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-bomb", "fa-leaf", "fa-bomb", "fa-bolt", "fa-bicycle", "fa-paper-plane-o", "fa-cube"]
@@ -33,6 +32,12 @@ function shuffle(array) {
 
     return array;
 }
+
+/*When user clock reset button, call resetGame fuction*/
+reset.addEventListener("click", resetGame);
+
+/* Setting event listener for click to every cards */
+setEventforClick();
 
 function setEventforClick() {
     for (let i = 0; i < allCards.length; i++) {
@@ -125,25 +130,3 @@ function incrementMoveCount() {
     moves++;
     movesShow.innerHTML = moves;
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-
-
-/****************************************
-         PROGRAM STARTS HERE
-****************************************/
-
-/*When user clock reset button, call resetGame fuction*/
-reset.addEventListener("click", resetGame);
-
-/* Setting event listener for click to every cards */
-setEventforClick();
